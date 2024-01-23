a7bb40148ada278e43e179327ae6147048cbceb0
laksamanaap <laksamana.arya1412@gmail.com>
2024-01-23 21:44:34 +0700

    feat : add keyboard func

--- App.js
+++ App.js
@@ -7,6 +7,9 @@ import {
   Text,
   StyleSheet,
   Image,
+  TouchableWithoutFeedback,
+  Keyboard,
+  ScrollView,
 } from "react-native";
 import AntDesign from "react-native-vector-icons/AntDesign";
 
@@ -16,90 +19,95 @@ export default function App() {
   const [password, setPassword] = useState("");
 
   return (
-    <View style={styles.container}>
-      <Image
-        style={{
-          width: 100,
-          height: 100,
-          resizeMode: "cover",
-          marginBottom: 45,
-        }}
-        source={require("./assets/icon/logo2.png")}
-      />
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Username</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Username"
-          value={username}
-          onChangeText={(text) => setUsername(text)}
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <ScrollView contentContainerStyle={styles.container}>
+        <Image
+          style={{
+            width: 100,
+            height: 100,
+            resizeMode: "cover",
+            marginBottom: 45,
+          }}
+          source={require("./assets/icon/logo2.png")}
         />
-      </View>
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Email</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Email"
-          value={email}
-          onChangeText={(text) => setEmail(text)}
-          keyboardType="email-address"
-        />
-      </View>
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Password</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Password"
-          value={password}
-          onChangeText={(text) => setPassword(text)}
-          secureTextEntry={true}
-        />
-      </View>
-      <TouchableOpacity style={styles.button}>
-        <Text style={{ color: "white", textAlign: "center", fontSize: 16 }}>
-          Daftar
-        </Text>
-      </TouchableOpacity>
-      <View>
-        <Text style={styles.OAuthDesc}>Atau masuk menggunakan</Text>
-      </View>
-      <View style={styles.OAuthContainer}>
-        <TouchableOpacity style={styles.OAuthButton}>
-          <AntDesign name="google" style={{ color: "#A9329D", fontSize: 20 }} />
-          <Text
-            style={{
-              color: "black",
-              textAlign: "center",
-              fontSize: 16,
-              fontWeight: "500",
-            }}
-          >
-            Google
-          </Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.OAuthContainer}>
-        <TouchableOpacity style={styles.OAuthButton}>
-          <AntDesign
-            name="facebook-square"
-            style={{ color: "#A9329D", fontSize: 20 }}
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Username</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Username"
+            value={username}
+            onChangeText={(text) => setUsername(text)}
           />
-          <Text
-            style={{
-              color: "black",
-              textAlign: "center",
-              fontSize: 16,
-              fontWeight: "500",
-            }}
-          >
-            Facebook
+        </View>
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Email</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Email"
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+            keyboardType="email-address"
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Password</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+            secureTextEntry={true}
+          />
+        </View>
+        <TouchableOpacity style={styles.button}>
+          <Text style={{ color: "white", textAlign: "center", fontSize: 16 }}>
+            Daftar
           </Text>
         </TouchableOpacity>
-      </View>
-      <View>
-        <Text style={styles.OAuthDesc}>Sudah punya akun? Login</Text>
-      </View>
-    </View>
+        <View>
+          <Text style={styles.OAuthDesc}>Atau masuk menggunakan</Text>
+        </View>
+        <View style={styles.OAuthContainer}>
+          <TouchableOpacity style={styles.OAuthButton}>
+            <AntDesign
+              name="google"
+              style={{ color: "#A9329D", fontSize: 20 }}
+            />
+            <Text
+              style={{
+                color: "black",
+                textAlign: "center",
+                fontSize: 16,
+                fontWeight: "500",
+              }}
+            >
+              Google
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.OAuthContainer}>
+          <TouchableOpacity style={styles.OAuthButton}>
+            <AntDesign
+              name="facebook-square"
+              style={{ color: "#A9329D", fontSize: 20 }}
+            />
+            <Text
+              style={{
+                color: "black",
+                textAlign: "center",
+                fontSize: 16,
+                fontWeight: "500",
+              }}
+            >
+              Facebook
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <Text style={styles.OAuthDesc}>Sudah punya akun? Login</Text>
+        </View>
+      </ScrollView>
+    </TouchableWithoutFeedback>
   );
 }
 
